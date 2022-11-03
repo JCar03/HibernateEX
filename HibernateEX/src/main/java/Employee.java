8e34f4dab8e0f435bc6e949ec8001d385100f06d
JCar03 <102256453+JCar03@users.noreply.github.com>
2022-11-03 13:32:22 -0500

    Add files via upload

---
+++ HibernateEX/src/main/java/Employee.java
@@ -0,0 +1,21 @@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Employee {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int salary;
+
+    public Employee(String firstName, String lastName, int salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+}
